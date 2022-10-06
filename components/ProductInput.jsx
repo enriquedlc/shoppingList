50475735f39e4c0c946891ca78d90140e2000d4f
Enrique <enriquedlc2d@gmail.com>
2022-10-06 09:27:09 +0100

    Feature(ProductInput): added State Management to ProductInput component

--- components/ProductInput.jsx
+++ components/ProductInput.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react"
 import { StyleSheet, TextInput, View, Button } from "react-native"
 
  const ProductInput = () => {
+
+    const [productName, setProductName] = useState('') // useState('') cuando creo la instancia va a haber una cadena vacía
+
+    const changeTextHandler = (value) => {
+        console.log(value)
+        setProductName(value) // lo que escribo se guerda en el estado
+    }
+
     return (
-        <View style={styles.productInput}>
-            <TextInput placeholder='Introduzca un producto' style={styles.productName}></TextInput>
-            <Button title="añadir" style={styles.addButton}></Button>
+        <View style={ styles.productInput }>
+            <TextInput 
+            style={ styles.productName }
+            placeholder='Introduzca un producto'
+            keyboardType="default" // manera de mostrar el teclado
+            onChangeText={ changeTextHandler } // onChangeText actualiza el texto
+            value={ productName } // value es lo que hay en el textInput
+            ></TextInput>
+            <Button title="añadir" style={ styles.addButton }></Button>
         </View>
     )
 }
